9ad2239c7dd5697d62d3a5e78feaa6b5f0bd7d63
lts <1833817576@qq.com>
2025-07-11 18:06:24 +0800

    env

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,8 +4,8 @@ import "github.com/zeromicro/go-zero/rest"
 
 type Config struct {
 	rest.RestConf
-	ClusterName        string `env:"CLUSTER_NAME"`
-	SchedulingPolicy   string `env:"SCHEDULING_POLICY"`
-	GpuTypeLabel       string `env:"GPU_TYPE_LABEL"`
-	AvaliableNodeLabel string `env:"AVALIABLE_NODE_LABEL"`
+	ClusterName        string `json:",env=CLUSTER_NAME"`
+	SchedulingPolicy   string `json:",env=SCHEDULING_POLICY"`
+	GpuTypeLabel       string `json:",env=GPU_TYPE_LABEL"`
+	AvaliableNodeLabel string `json:",env=AVALIABLE_NODE_LABEL"`
 }
